b8b54b5ce61b61743250607d9de2d8bdae6e87a7
Eric Drechsel <eric@pdxhub.org>
2016-01-26 12:36:52 -0800

    firm up path handling

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 "use strict";
 
 var args = require('minimist')(process.argv.slice(2), {boolean: ['debug','h']});
+var fs = require('fs')
+var path = require('path')
 var jsDAV = require("jsdav");
 var jsDAV_Locks_Backend_FS = require("jsdav/lib/DAV/plugins/locks/fs");
 
@@ -20,7 +22,9 @@ function exitMsg(msg, code){
 if (args.h || args._.length !== 1) exitMsg(usage);
 if (args.debug) jsDAV.debugMode = true;
 
-var rootDir = __dirname + args._.shift();
+var rootDir = path.join(args._.shift(), '/');
+if (!fs.existsSync(rootDir)) exitMsg(rootDir+" does not exist.");
+if (!fs.statSync(rootDir).isDirectory()) exitMsg(rootDir+": not a directory.");
 jsDAV.createServer({
   node: rootDir,
   locksBackend: jsDAV_Locks_Backend_FS.new(rootDir)
