5fc806c1d49a3a8004b35269cf85f68fe448a2e2
Eric Drechsel <eric@pdxhub.org>
2016-01-26 11:19:56 -0800

    Add opt parse
    Closes #2

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 /*
  * @package dav-server
- * @copyright Copyright(c) 2016 PDXHub.org <eric AT PDXHub.org>
+ * @copyright Copyright(c) 2016 PDXHub <eric AT PDXHub.org>
  * @author Eric Drechsel <eric AT PDXHub.org>
  * @license http://github.com/edrex/dav-server/blob/master/LICENSE MIT License
  */
 "use strict";
 
+var args = require('minimist')(process.argv.slice(2), {boolean: ['debug','h']});
 var jsDAV = require("jsdav");
-jsDAV.debugMode = true;
 var jsDAV_Locks_Backend_FS = require("jsdav/lib/DAV/plugins/locks/fs");
 
-var rootDir = __dirname + "";
+function printUsage(){
+  console.log("dav-server [--debug] rootDir");
+  process.exit();
+}
+
+if (args.h || args._.length !== 1) printUsage();
+if (args.debug) jsDAV.debugMode = true;
+
+var rootDir = __dirname + args._.shift();
 jsDAV.createServer({
   node: rootDir,
   locksBackend: jsDAV_Locks_Backend_FS.new(rootDir)
